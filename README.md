cace8a709c45b39b6d920410db7403443c0989fb
Reed Wilson <reedcwilson@gmail.com>
2017-01-16 15:01:22 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+### Mad Libs - A lesson on dictionaries
+
+For an explanation of the assignment and requirements go to the [Programming
+Fundamentals](https://programming.reedcwilson.com/mad-libs.html) website.
+
---
+++ languageparts.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# import modules here
+
+# Create a dictionary of language parts. It must contain: noun, verb, adjective.
+# The key should be one of the types of language (e.g. noun) and the value
+# should be the list of words that you choose.
+
+# this is a function that you can learn about in the functions chapter
+def get_word_part(part):
+    # put your code to get a language part here
+    # make sure that you return the language part e.g.
+    return language_part
+
