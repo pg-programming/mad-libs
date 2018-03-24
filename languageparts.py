e46567b1153ea8264e61b9368c1efc9c26cbd360
Reed Wilson <reedcwilson@gmail.com>
2018-03-24 09:00:04 -0600

    update name of return value

--- languageparts.py
+++ languageparts.py
@@ -7,8 +7,8 @@
 # should be the list of words that you choose.
 
 # this is a function that you can learn about in the functions chapter
-def get_word_part(part):
+def get_word_part(part): 
     # put your code to get a language part here
     # make sure that you return the language part e.g.
-    return language_part
+    return word
 
