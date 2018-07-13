5647a41916e0f03eb504e898bda822f957b280af
Reed Wilson <rwilson@ancestry.com>
2018-07-13 11:51:52 -0600

    python3ify

--- mad-libs.py
+++ mad-libs.py
@@ -5,8 +5,10 @@ import languageparts
 import re
 from random import randint
 
+
 def get_token(word):
     return re.match("(%[a-zA-Z ]*%)", word, re.I)
+
 
 def build_lib(lib):
     new_lib = []
@@ -16,24 +18,30 @@ def build_lib(lib):
         match_obj = get_token(word)
         if match_obj:
             token = match_obj.group(1)
-            new_word = word.replace(token, languageparts.get_word_part(token.replace('%', '')))
+            new_word = word.replace(
+                    token,
+                    languageparts.get_word_part(token.replace('%', ''))
+            )
         else:
             new_word = word
         new_lib.append(new_word)
     return " ".join(new_lib).strip()
 
+
 def get_random_lib():
     libs = []
     with open("libs.txt") as f:
-        libs = [ l for l in f.readlines() if l.strip() ]
-    return libs[randint(0, len(libs)-1)] 
+        libs = [l for l in f.readlines() if l.strip()]
+    return libs[randint(0, len(libs)-1)]
+
 
 def main():
     os.system('clear')
     lib = get_random_lib()
     new_lib = build_lib(lib)
-    print "original:", lib.strip()
-    print "mad libs:", new_lib.strip()
+    print("original:", lib.strip())
+    print("mad libs:", new_lib.strip())
+
 
 if __name__ == "__main__":
     main()
