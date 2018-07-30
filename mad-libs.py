bd50292a4427065352ff084a0b9da8ecef11b707
Reed Wilson <rwilson@ancestry.com>
2018-07-30 10:37:59 -0600

    shorten mad-libs.py

--- mad-libs.py
+++ mad-libs.py
@@ -1,47 +1,30 @@
 #!/usr/bin/env python
 
 import os
-import languageparts
+import languageparts as lp
 import re
-from random import randint
-
-
-def get_token(word):
-    return re.match("(%[a-zA-Z ]*%)", word, re.I)
+from random import choice
 
 
 def build_lib(lib):
     new_lib = []
-    words = lib.strip('\n').split(" ")
-    for word in words:
-        new_word = None
-        match_obj = get_token(word)
+    for w in lib.strip('\n').split(" "):
+        match_obj = re.match("(%[a-zA-Z ]*%)", w, re.I)
         if match_obj:
             token = match_obj.group(1)
-            new_word = word.replace(
-                    token,
-                    languageparts.get_word_part(token.replace('%', ''))
-            )
-        else:
-            new_word = word
-        new_lib.append(new_word)
+            w = w.replace(token, lp.get_word_part(token.replace('%', '')))
+        new_lib.append(w)
     return " ".join(new_lib).strip()
 
 
 def get_random_lib():
-    libs = []
     with open("libs.txt") as f:
-        libs = [l for l in f.readlines() if l.strip()]
-    return libs[randint(0, len(libs)-1)]
+        return choice([l for l in f.readlines() if l.strip()])
 
 
-def main():
+if __name__ == "__main__":
     os.system('clear')
     lib = get_random_lib()
     new_lib = build_lib(lib)
     print("original:", lib.strip())
     print("mad libs:", new_lib.strip())
-
-
-if __name__ == "__main__":
-    main()
